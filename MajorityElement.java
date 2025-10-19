67208a9a149cf9368c5251adafe5444a86429ee5
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-10-19 13:28:13 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,24 @@
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement sol = new MajorityElement();
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println(sol.majorityElement(nums1)); // Output: 3
+        System.out.println(sol.majorityElement(nums2)); // Output: 2
+    }
+}
