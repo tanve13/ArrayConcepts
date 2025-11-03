5d6eff71f7b6caa90b5b1f9a9443832ca8452fb6
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-11-03 20:10:54 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -9,3 +9,32 @@ public class MissingNumber{
         System.out.println("Missing Number is: "+total);
     }
 }
+//cam be done in this way also
+ class MissingNumbers {
+
+    static class Solution {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int expectedSum = n * (n + 1) / 2;
+            int actualSum = 0;
+
+            for (int num : nums) {
+                actualSum += num;
+            }
+
+            return expectedSum - actualSum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {3, 0, 1};
+        int[] nums2 = {0, 1};
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+
+        System.out.println("Missing number in [3,0,1] → " + sol.missingNumber(nums1));
+        System.out.println("Missing number in [0,1] → " + sol.missingNumber(nums2));
+        System.out.println("Missing number in [9,6,4,2,3,5,7,0,1] → " + sol.missingNumber(nums3));
+    }
+}
