864053eb53ad481cf4e1659fee93f24468f2faa8
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-11-27 19:01:22 +0530

    Create FindFirstMissingPositive.java

---
+++ FindFirstMissingPositive.java
@@ -0,0 +1,34 @@
+public class FindFirstMissingPositive {
+
+    static class Solution {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= 0 || nums[i] > n) {
+                    nums[i] = n + 1;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                int val = Math.abs(nums[i]);
+                if (val <= n) {
+                    nums[val - 1] = -Math.abs(nums[val - 1]);
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (nums[i] > 0) return i + 1;
+            }
+
+            return n + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {3, 4, -1, 1};
+        System.out.println(sol.firstMissingPositive(nums)); // Output: 2
+    }
+}
