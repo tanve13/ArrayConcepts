42cda0b8c5f87bcb3a846545ff3edb1c41be92a5
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-12-25 21:05:07 +0530

    Update FindPeakElement.java

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,6 +1,5 @@
 public class FindPeakElement {
-
-    static class Solution {
+static class Solution {
         public int findPeakElement(int[] nums) {
             int n = nums.length;
             for (int i = 0; i < n - 1; i++) {
