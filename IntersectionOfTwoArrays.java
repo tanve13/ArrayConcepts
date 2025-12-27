adc0d6210ab1e9fb5950b0314c1c989a3cf2880a
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-12-27 22:12:29 +0530

    Create IntersectionOfTwoArrays.java

---
+++ IntersectionOfTwoArrays.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class IntersectionOfTwoArrays {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        int[] result = solution.intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result)); // [2]
+    }
+}
+
+class Solution {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+}
