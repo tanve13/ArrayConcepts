14ed81d2da157561d292590add24de20798bc3f0
tanve13 <171332553+tanve13@users.noreply.github.com>
2026-02-05 22:34:06 +0530

    Update IntersectionOfTwoArrays.java

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -7,7 +7,7 @@ public class IntersectionOfTwoArrays {
         int[] nums1 = {1, 2, 2, 1};
         int[] nums2 = {2, 2};
 
-        int[] result = solution.intersection(nums1, nums2);
+int[] result = solution.intersection(nums1, nums2);
         System.out.println(Arrays.toString(result)); // [2]
     }
 }
